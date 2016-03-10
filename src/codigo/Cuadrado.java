a3aaaa1711f7eb120044ea7024038f750dd377c4
familia <familia@172.27.2.48>
2016-03-10 19:03:41 +0100

    Me cago en github

--- src/codigo/Cuadrado.java
+++ src/codigo/Cuadrado.java
@@ -1,36 +1,48 @@
+
 /*
-* Mi primera clase java
- 
-La clase círculo permite almacenar un círculo
+ * Mi segunda clase java
+
+La clase triangulo permite almacenar un triangulo
 además de su posición en la pantalla, si está relleno o no
 , su color etc
-*/
+ */
 package codigo;
- 
+
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
- 
+import java.awt.Polygon;
+import java.awt.Stroke;
+
 /**
-*
-* @author Jorge Cisneros
-*/
-public class Cuadrado extends Rectangle2D.Double{
-   
+ *
+ * @author Jorge Cisneros
+ */
+public class Cuadrado extends Polygon{
+    
     public Color color = null;
     public boolean relleno = false;
+    public Stroke grosor = null;
     
     public Cuadrado (int _x, int _y, int _width,  Color _color, boolean _relleno){
-        this.x = _x - _width/2;
-        this.y = _y - _width/2;
-        this.width = _width;
-        this.height = _width;
+
+        this.npoints = 4;
+        this.xpoints[0] = _x - _width/2;
+        this.ypoints[0] = _y - _width/2; 
+
+        this.xpoints[1] = _x - _width/2;
+        this.ypoints[1] = _y + _width/2;
+ 
+        this.xpoints[2] = _x + _width/2;
+        this.ypoints[2] = _y + _width/2;
+        
+        this.xpoints[3] = _x + _width/2;
+        this.ypoints[3] = _y - _width/2;
+        
         this.color = _color;
         this.relleno = _relleno;
-       
+        
     }
-   
-    public void pintaYColorea(Graphics2D g2){
+        public void pintaYColorea(Graphics2D g2){
             g2.setColor(this.color);
             if (this.relleno) {
                 g2.fill(this);
